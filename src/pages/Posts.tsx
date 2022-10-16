d3a6bc1d9a951e26b6ba4f8e96e0a664b0eb9781
Bartosz Dudziński <bartoszdudzinski18@gmail.com>
2022-10-16 21:43:19 +0200

    added basic ui with posts searching

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,55 +1,71 @@
-import { Component, For } from "solid-js";
+import { useNavigate } from "solid-app-router";
+import { Component, For, createContext, createSignal, Accessor, Setter, onMount, createEffect, Show } from 'solid-js';
 import { styled } from 'solid-styled-components';
-import { PostI } from "../App";
-import { Post } from "./Posts/Post";
+import { PostI, getPosts, searchPostByTitle } from '../App';
+import { PostCard } from "./Posts/PostCard";
 
 export const PostsPage: Component = () => {
 
-	const a: PostI[] = [
-		{
-			id: 1,
-			desc: "desc 1",
-			geolocation: {
-				lat: 1,
-				lng: 1
-			},
-			created_at: new Date(),
-			images: [],
-			title: "title 1"
-		},
-		{
-			id: 2,
-			desc: "desc 2",
-			geolocation: {
-				lat: 1,
-				lng: 1
-			},
-			created_at: new Date(),
-			images: [],
-			title: "title 2"
-		},
-		{
-			id: 3,
-			desc: "desc 3",
-			geolocation: {
-				lat: 1,
-				lng: 1
-			},
-			created_at: new Date(),
-			images: [],
-			title: "title 3"
+	const navigate = useNavigate()
+
+	const [ getPosts, setPosts ]: [ Accessor<PostI[]>, Setter<PostI[]> ] = createSignal([])
+
+
+	const [ searchInput, setSearchInput ] = createSignal("")
+
+	let typingTimer: NodeJS.Timeout
+	const doneTypingInterval = 1000
+
+	let inputElem = document.getElementById("search-input")
+	onMount(() => {
+		inputElem = document.getElementById("search-input")
+		// console.log(worker)
+	})
+
+	const stoppedTyping = () => {
+		// @ts-expect-error
+		setSearchInput(inputElem.value)
+	}
+
+	createEffect(() => {
+		if (searchInput()) {
+
+			searchPostByTitle(searchInput()).then((r) => {
+				console.log(r)
+				setPosts(r)
+			})
+
+		} else {
+			setPosts([])
 		}
-	]
+	})
+
+	createEffect(() => {
+		console.log(getPosts())
+	})
 
 	return (
 		<>
 			<PostsPageStyle>
-				<For each={a}>
-					{(post) => (
-						<Post post={post} />
-					)}
-				</For>
+				<div id="search-bar">
+					<input id="search-input" onInput={(e) => {
+						clearTimeout(typingTimer)
+						typingTimer = setTimeout(stoppedTyping, doneTypingInterval)
+					}}></input>
+				</div>
+				{/* <Show when={getPosts().length > 0} fallback={(<span>No posts :(</span>)}> */}
+				<For each={getPosts()}>
+					{(post) => {
 
+						return (
+							<a href={`/post/${post.id}`}>
+								<PostCard post={post} />
+							</a>
+						)
+					}}
+				</For>
+				<div id="bottom-space"></div>
+				{/* </Show> */}
 			</PostsPageStyle>
 		</>
 	)
@@ -57,9 +73,20 @@ export const PostsPage: Component = () => {
 
 const PostsPageStyle = styled("div")(() => {
 	return {
+		overflow: "scroll",
 		height: "100%",
 		width: "100%",
 		display: "flex",
-		flexDirection: "column"
+		flexDirection: "column",
+
+		"#search-bar": {
+			position: "sticky",
+			top: "0"
+		},
+
+		"#bottom-space": {
+			width: "100%",
+			height: "25vh"
+		}
 	}
 })
