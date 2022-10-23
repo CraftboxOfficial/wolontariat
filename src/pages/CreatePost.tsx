660e69f0c81bcec78b58e6b83cc9dac9db0ca77a
Bartosz Dudziński <bartoszdudzinski18@gmail.com>
2022-10-23 17:13:48 +0200

    new post new style

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,4 +1,4 @@
-import { Accessor, Component, createEffect, createSignal, onMount, Setter, Show } from 'solid-js';
+import { Accessor, Component, createEffect, createSignal, JSXElement, onMount, Setter, Show } from 'solid-js';
 import { styled } from 'solid-styled-components';
 import { FetchedPosts, getPosts } from '../App';
 import { BackButton } from '../components/BackButton';
@@ -15,101 +15,103 @@ export const CreatePostPage: Component = (props) => {
 	});
 
 	const [ selectedImage, setSelectedImage ] = createSignal()
-	const [isUploading, setIsUploading] = createSignal(false)
+	const [ isUploading, setIsUploading ] = createSignal(false)
 	const [ insertDesc, setInsertDesc ] = createSignal('');
 	const [ insertTitle, setInsertTitle ] = createSignal('');
 	const [ insertAddress, setInsertAddress ] = createSignal('');
-	const [insertResult, setInsertResult] = createSignal(null);
+	const [ insertResult, setInsertResult ] = createSignal(null);
 	const MAXIMUM_FILE_SIZE = 1000000; //1 mb
 
 	createEffect(() => {
 		console.log(selectedImage())
 	})
 
-	const insertPost = async (text: string, desc: string, loc: {lat: number, lng: number}, images: any[], address: string) => {
-    const data = await supabase.from('posts').insert({ title: text, geolocation: loc, desc: desc, images: images, address: address });
-    if(data.error){
-      //@ts-ignore
-      setInsertResult({"data": null, "error": 'Database access denied'});
-      return null;
-    }
-    //@ts-ignore
-    setInsertResult({"data": data, "error": null});
-    return data;
-  }
+	const insertPost = async (text: string, desc: string, loc: { lat: number, lng: number }, images: any[], address: string) => {
+		const data = await supabase.from('posts').insert({ title: text, geolocation: loc, desc: desc, images: images, address: address });
+		if (data.error) {
+			//@ts-ignore
+			setInsertResult({ "data": null, "error": 'Database access denied' });
+			return null;
+		}
+		//@ts-ignore
+		setInsertResult({ "data": data, "error": null });
+		return data;
+	}
 
-	function formatFileSize(bytes:any, decimalPoint:any) {
-    if(bytes == 0) return '0 Bajtów';
-    var k = 1000,
-        dm = decimalPoint || 2,
-        sizes = ['Bajtów', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
-        i = Math.floor(Math.log(bytes) / Math.log(k));
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-  }
+	function formatFileSize(bytes: any, decimalPoint: any) {
+		if (bytes == 0) return '0 Bajtów';
+		var k = 1000,
+			dm = decimalPoint || 2,
+			sizes = [ 'Bajtów', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB' ],
+			i = Math.floor(Math.log(bytes) / Math.log(k));
+		return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[ i ];
+	}
 
 	/**
    * It takes a file, uploads it to supabase, and returns the public url of the file
    * @param {any} file - the file to be uploaded
   */
-		const uploadFile = async (file:any) => {
-			if(file === null)
-				return;  
-	
-			// Max: 1mb
-			if(file.size > MAXIMUM_FILE_SIZE){
-				setIsUploading(false);
-				//@ts-ignore
-				setInsertResult({"data": null, "error": 'File too large'});
-				throw new Error("File size too large")
-			}
-	
-			const fileName = file.name;
-			const fileSize = formatFileSize(file.size, 2);
-	
-			const fileExtension = (/[.]/.exec(fileName)) ? /[^.]+$/.exec(fileName) : undefined;
-			const randomName = crypto.randomUUID();
-	
-			await supabase.storage.from('images').upload('public/' + randomName + "." + fileExtension, file, {
-				cacheControl: '3600',
-				upsert: true
-			}).then((res) => {
-				if(res.error){
-					setIsUploading(false);
-					//@ts-ignore
-					setInsertResult({"data": null, "error": 'File upload failed'});
-					throw new Error("File upload failed")
-				}
-			})
-	
-			const {data} = await supabase.storage.from('images').getPublicUrl('public/' + randomName + "." + fileExtension);
-			return data.publicUrl;
+	const uploadFile = async (file: any) => {
+		if (file === null)
+			return;
+
+		// Max: 1mb
+		if (file.size > MAXIMUM_FILE_SIZE) {
+			setIsUploading(false);
+			//@ts-ignore
+			setInsertResult({ "data": null, "error": 'File too large' });
+			throw new Error("File size too large")
 		}
 
-		const uploadHandler = async () => {
-			setIsUploading(true);
-			if(navigator.geolocation){
-				await navigator.geolocation.getCurrentPosition(async (position) => {
-					await uploadFile(selectedImage()).then(async (url) => {
-						await insertPost(insertTitle(), insertDesc(), {"lat": position.coords.latitude, "lng": position.coords.longitude}, url !== undefined ? [url] : [], insertAddress()).then((res) => {
-							if (res.error)
-								console.error(res.error);
-				
-							setIsUploading(false);
-						})
-					})
-				}, (error) => {
-					console.error(error);
-				});
-			}else{
-				console.error('Browser does not support geolocation');
+		const fileName = file.name;
+		const fileSize = formatFileSize(file.size, 2);
+
+		const fileExtension = (/[.]/.exec(fileName)) ? /[^.]+$/.exec(fileName) : undefined;
+		const randomName = crypto.randomUUID();
+
+		await supabase.storage.from('images').upload('public/' + randomName + "." + fileExtension, file, {
+			cacheControl: '3600',
+			upsert: true
+		}).then((res: any) => {
+			if (res.error) {
+				setIsUploading(false);
+				//@ts-ignore
+				setInsertResult({ "data": null, "error": 'File upload failed' });
+				throw new Error("File upload failed")
 			}
+		})
+
+		const { data } = await supabase.storage.from('images').getPublicUrl('public/' + randomName + "." + fileExtension);
+		return data.publicUrl;
+	}
+
+	const uploadHandler = async () => {
+		setIsUploading(true);
+		if (navigator.geolocation) {
+			await navigator.geolocation.getCurrentPosition(async (position) => {
+				await uploadFile(selectedImage()).then(async (url) => {
+					await insertPost(insertTitle(), insertDesc(), { "lat": position.coords.latitude, "lng": position.coords.longitude }, url !== undefined ? [ url ] : [], insertAddress()).then((res) => {
+						if (res.error)
+							console.error(res.error);
+
+						setIsUploading(false);
+					})
+				})
+			}, (error) => {
+				console.error(error);
+			});
+		} else {
+			console.error('Browser does not support geolocation');
 		}
+	}
 
 	return (
 		<>
 			<CreatePostStyle>
-				<div id="top">
+				<div id="back-button">
 					<BackButton />
+				</div>
+				<div id="top">
 					<span id="page-title">Dodaj ogłoszenie</span>
 				</div>
 				<div id="image-input-div" onClick={(e) => {
@@ -136,14 +138,14 @@ export const CreatePostPage: Component = (props) => {
 				</div>
 
 				<Show when={insertResult() !== null}>
-          {/*@ts-ignore*/}
-          {insertResult().error !== null && <h5 style={{color: 'tomato'}}>{insertResult().error}</h5>}
-          {/*@ts-ignore*/}
-          {insertResult().data !== null && <h5 style={{color: 'lightgreen'}}>{"Success"}</h5>}
-        </Show>
-				<input id="title-input" type='text' value={insertTitle()} onInput={(e) => {setInsertTitle(e.target.value)}}></input>
-				<input id="address-input" type='text' value={insertAddress()} onInput={(e) => {setInsertAddress(e.target.value)}}></input>
-				<textarea id="description-input" value={insertDesc()} onInput={(e) => {setInsertDesc(e.target.value)}}></textarea>
+					{/*@ts-ignore*/}
+					{insertResult().error !== null && <h5 style={{ color: 'tomato' }}>{insertResult().error}</h5>}
+					{/*@ts-ignore*/}
+					{insertResult().data !== null && <h5 style={{ color: 'lightgreen' }}>{"Success"}</h5>}
+				</Show>
+				<input id="title-input" type='text' placeholder="Tytuł..." value={insertTitle()} onInput={(e) => { setInsertTitle(e.target.value) }}></input>
+				<input id="address-input" type='text' placeholder="Lokalizacja..." value={insertAddress()} onInput={(e) => { setInsertAddress(e.target.value) }}></input>
+				<textarea id="description-input" placeholder="Opis..." value={insertDesc()} onInput={(e) => { setInsertDesc(e.target.value) }}></textarea>
 				<button id="submit-post" onClick={uploadHandler}>Dodaj ogłoszenie</button>
 			</CreatePostStyle>
 		</>
@@ -152,31 +154,41 @@ export const CreatePostPage: Component = (props) => {
 
 const CreatePostStyle = styled("div")(() => {
 	return {
-		width: "100%",
+		margin: "auto",
+		// width: "100%",
+		height: "100%",
+		maxWidth: "640px",
 		display: "flex",
 		flexDirection: "column",
 
+		"#back-button": {
+			position: "absolute",
+			margin: "20px 0"
+			// aspectRatio: "1 / 1",
+			// // width: "20%",
+			// // maxWidth: "80px",
+			// margin: "5%",
+			// // borderRadius: "25%",
+			// border: "none"
+		},
+
 		"#top": {
+			margin: "20px 0",
+			height: "50px",
 			display: "flex",
 			flexDirection: "row",
 
-			button: {
-				aspectRatio: "1 / 1",
-				// width: "20%",
-				// maxWidth: "80px",
-				margin: "5%",
-				// borderRadius: "25%",
-				border: "none"
-			},
-
-			alignItems: "center",
+			justifyContent: "center",
 
 			"#page-title": {
-				fontSize: "180%"
+				fontSize: "180%",
+				alignSelf: "center",
+				fontWeight: "bolder"
 			}
 		},
 
 		"#image-input-div": {
+			marginTop: "20px",
 			display: "block",
 			// outlineOffset: "-4px",
 			border: "6px dashed",
@@ -214,5 +226,70 @@ const CreatePostStyle = styled("div")(() => {
 		},
 
 
+		"#title-input": {
+			marginTop: "20px",
+			backgroundColor: "#383838",
+			border: "none",
+			borderRadius: "10px",
+			height: "48px",
+			minHeight: "48px",
+			fontSize: "140%",
+			padding: "0 20px",
+			color: "#FFFFFF"
+		},
+
+		"#title-input:focus": {
+			outline: "none"
+		},
+
+		"#address-input": {
+			marginTop: "20px",
+			backgroundColor: "#383838",
+			border: "none",
+			borderRadius: "10px",
+			height: "48px",
+			minHeight: "48px",
+			fontSize: "140%",
+			padding: "0 20px",
+			color: "#FFFFFF"
+		},
+
+		"#address-input:focus": {
+			outline: "none"
+		},
+
+		"#description-input": {
+			marginTop: "20px",
+			backgroundColor: "#383838",
+			border: "none",
+			resize: "none",
+			borderRadius: "10px",
+			height: "200px",
+			minHeight: "48px",
+			fontSize: "140%",
+			padding: "20px 20px",
+			color: "#FFFFFF",
+			fontFamily: "Arial"
+		},
+
+		"#description-input:focus": {
+			outline: "none"
+		},
+
+		"#submit-post": {
+			position: "fixed",
+			bottom: "0",
+			width: "100%",
+			maxWidth: "640px",
+			// marginLeft: "auto",
+			// marginRight: "auto",
+			// width: "calc(100% - 20px)",
+			marginTop: "auto",
+			marginBottom: "20px",
+			minHeight: "48px",
+			borderRadius: "10px",
+			border: "none",
+			fontSize: "140%"
+		},
 	}
 })
